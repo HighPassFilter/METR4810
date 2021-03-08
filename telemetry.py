5ed2ece233f1384e35b5a1b93c0cf13a330d419a
HighPassFilter <alexanderyue99@gmail.com>
2021-03-08 07:57:54 +0000

    Addition of telemetry script

---
+++ telemetry.py
@@ -0,0 +1,9 @@
+import socket
+
+
+# Create INET socket and stream data 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Details about server
+host = # To be obtained dynamically?
+port = 5454
