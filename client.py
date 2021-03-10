81fd4b61642d15281f4025fd58e0a632ccae5fcc
HighPassFilter <alexanderyue99@gmail.com>
2021-03-10 11:55:37 +0000

    Update client script

--- client.py
+++ client.py
@@ -1,9 +1,26 @@
 import socket
 
+# Details about server
+host = "192.168.0.3" # To be obtained dynamically?
+port = 65432
 
 # Create INET socket and stream data 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((host, port))
 
-# Details about server
-host = # To be obtained dynamically?
-port = 5454
+    while True:
+        command = input("Enter your command: ")
+
+        if command == 'EXIT':
+            # Send exit request to the other end
+            s.send(str.encode(command))
+            break
+        else:
+            s.send(str.encode(command))
+            reply = s.recv(1024)
+            print(reply.decode("utf-8"))
+
+
+
+
+
