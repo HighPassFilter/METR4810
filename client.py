87ebe665ce680d1389c6984e8ed5f64c0452f938
HighPassFilter <alexanderyue99@gmail.com>
2021-03-10 21:53:42 +1000

    Socket example using TCP packet stream

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
 import socket
+import logging
 
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
 
-# Create INET socket and stream data 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
 
-# Details about server
-host = # To be obtained dynamically?
-port = 5454
+print('Received', repr(data))
