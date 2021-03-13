55242e63bffd353e99151d6da3c551b5887f9e20
HighPassFilter <alexanderyue99@gmail.com>
2021-03-13 06:41:14 +0000

    Worked on robot commands

--- Robot.py
+++ Robot.py
@@ -8,16 +8,27 @@ class Robot():
         # Setup navigation
 
         # Setup pilot
+    def receiveData(self):
+        message = self.server.receiveData()
+        if message == "abort":
+            self.abort()
+        if message == "release":
+            self.release()
+        if message == "shutdown":
+            print("HI")
+            self.shutDown()
 
+        return message
 
     def shutDown(self):
         # TODO: Shut everything down
-        self.server.closeConnection
+        print("Robot shutting down")
+        self.server.closeConnection()
 
     def release(self):
         # TODO: Communicate with pilot to release the clamp
-        hi = ""
+        print("Release!")
 
     def abort(self):
         # TODO: Turn off the thrusters, camera, sensors 
-        hi = ""
+        print("Abort!")
