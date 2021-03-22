1592f1c1bc6e17f3e9085c2183dffe4566eb594b
HighPassFilter <alexanderyue99@gmail.com>
2021-03-22 11:28:21 +1000

    partial system implementation

---
+++ GUI.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+class UserInterface():
+    def __init__(self):
+        # Initialise states
+        
+        # Setup terminal for receiving user inputs
+        self.terminal = UserInput()
+        self.terminal.start()
+        
+        #
+
+# This class controls the states of the user interface
+class UserInput(Thread):
+    # Requirements:
+    # 1. A way to communicate with the groundstation -> Setup the process of the groundStation (pipe)
+
+    def __init__(self):
+        self.queue = Queue()
+        Thread.__init__(self)
+
+    def run(self):
+        host = "192.168.137.184"#input("Please enter the robot's host ip address: ")
+        
+        # Return host
+        self.queue.put(host)
+
+        while True:
+            option = input("Please enter your command: ")
+
+            # Return command
+            self.queue.put(int(option))
+            if int(option) == 3:
+                break
+    
+    def getCommand(self):
+        return self.queue.get()
+class 
--- GroundStation.py
+++ GroundStation.py
@@ -1,35 +1,25 @@
 # Telemetry libraries
 from WiFi import Client
 
-
-
 # Threading libraries
 from queue import Queue
 from threading import Thread
+from multiprocessing import Process, Pipe
 class GroundStation():
-    def __init__(self):
+    def __init__(self, host):
         # Initialise class methods
         self.releaseFlag = 0
-        # Setup user interface
-        self.user = UserInterface()
+        self.isShutDown = 0
+        self.main_pipe, self.tele_pipe = Pipe()
 
-        # Begin the user interface and retrieve host information
-        self.user.start()
-        host = self.user.queue.get()
-
-        # Setup client connection to robot
-        self.client = Client(host)
-
-        # Request user for release command
-        option = self.user.queue.get()
-        while int(option) != 2:
-            print("Please press 2 to release the robot")
-            option = self.user.queue.get()
-        
-        self.command(option)
+        # Setup new process
+        self.process = Process(target=telemetryProcess, args=(self))
+        self.process.start()
 
     def shutDown(self):
         self.client.closeConnection()
+        self.isShutDown = 1
+        self.process.join()
 
     def command(self, option):
         if option == 1:
@@ -55,50 +45,30 @@ class GroundStation():
         data = self.client.receiveData()
         return self.client.unpackData(data)
 
-    def gen_wrapper(self):
-        while True:
-            data = self.receiveRobotData()
-            yield data
+def telemetryProcess(groundStation):
+    try:
+        while groundStation.isShutDown == 0:
+            # Stage 1: Setup
+            # ----------------------------------------------------
+            # Setup user listener
+            
+            # Wait for IP address from user listener
 
-class UserInterface(Thread):
-    def __init__(self):
-        self.queue = Queue()
-        Thread.__init__(self)
+            # Setup client connection to robot
+            self.client = Client(host)
 
-    def run(self):
-        host = "192.168.137.184"#input("Please enter the robot's host ip address: ")
-        
-        # Return host 
-        self.queue.put(host)
+            # Wait for release command from user listener
 
-        while True:
-            option = input("Please enter your command: ")
+            # Stage 2: Descent
+            # ----------------------------------------------------
+            # Check for user inputs
+            
+            # 
+            
 
-            # Return command
-            self.queue.put(int(option))
-            if int(option) == 3:
-                break
-    
+    except Exception as e:
+        print(e)
 
-def run(data):
-    data = groundStation.re
-    # update the data
-    xdata.append(data[0])
-    y1data.append(data[1])
-    y2data.append(data[2])
-
-    # axis limits checking. Same as before, just for both axes
-    for ax in [ax1, ax2]:
-        xmin, xmax = ax.get_xlim()
-        if t >= xmax:
-            ax.set_xlim(xmin, 2*xmax)
-            ax.figure.canvas.draw()
-
-    # update the data of both line objects
-    line[0].set_data(xdata, y1data)
-    line[1].set_data(xdata, y2data)
-
-    return line
 
 def controller():
     if keyboard.is_pressed("1") and keyboard.is_pressed("="):
@@ -112,5 +82,8 @@ def controller():
         return 3
     return 0
 
+
+def commsUnit():
+    while True:
 
 
