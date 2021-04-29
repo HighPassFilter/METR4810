c75c1eb3b9af385516b063e36b86da5a6ec9da3a
HighPassFilter <alexanderyue99@gmail.com>
2021-04-29 16:17:24 +1000

    Add temperature + pressure API to telemetry class

--- Telemetry.py
+++ Telemetry.py
@@ -2,26 +2,32 @@
 import time
 import numpy as np
 import adafruit_bno055
+import adafruit_bmp280
 from busio import I2C
 from board import SDA, SCL
 
 # Note: The interface for the telemetry class should be the same even if we're using the same sensor
 class Telemetry():
     def __init__(self):
-        # Setup the IMU sensor
+        
         i2c = I2C(SCL, SDA)
-        self.sensor = adafruit_bno055.BNO055_I2C(i2c)
+
+        # Setup the IMU sensor
+        self.bno055 = adafruit_bno055.BNO055_I2C(i2c)
+
+        # Setup the temp + pressure sensor
+        self.bmp280 = 
 
     def getLinearAcceleration(self):
-        return self.sensor.linear_acceleration # Do we need raw acceleration or linear acceleration for the final demo?
+        return self.bno055.linear_acceleration # Do we need raw acceleration or linear acceleration for the final demo?
     
     def getOrientation(self):
-        return self.sensor.euler
+        return self.bno055.euler
 
     def getTemperature(self):
-        return 0
+        return bmp280.temperature
 
     def getPressure(self):
-        return 10
+        return bmp280.pressure
 
 
