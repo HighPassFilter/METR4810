bb1af12bf88d5339088ef153d2c27a2754ed7f12
HighPassFilter <alexanderyue99@gmail.com>
2021-04-29 18:37:53 +1000

    Addition of exceptions to handle sensor errors

--- Telemetry.py
+++ Telemetry.py
@@ -11,23 +11,45 @@ class Telemetry():
     def __init__(self):
         
         i2c = I2C(SCL, SDA)
+        try:
+            # Setup the IMU sensor
+            self.bno055 = adafruit_bno055.BNO055_I2C(i2c)
+            self.bno055_isPresent = True
 
-        # Setup the IMU sensor
-        self.bno055 = adafruit_bno055.BNO055_I2C(i2c)
-
-        # Setup the temp + pressure sensor
-        self.bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address = 0x76)
+        except Exception:
+            self.bno055_isPresent = False
+        
+        try:
+            # Setup the temp + pressure sensor
+            self.bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address = 0x76)
+            self.bmp280_isPresent = True
+        except Exception:
+            # Only IMU connected over I2C
+            self.bmp280_isPresent = False
 
     def getLinearAcceleration(self):
-        return self.bno055.linear_acceleration # Do we need raw acceleration or linear acceleration for the final demo?
+        if self.bno055_isPresent:
+            return self.bno055.linear_acceleration # Do we need raw acceleration or linear acceleration for the final demo?
+        else:
+            return [0, 0, 0]
     
     def getOrientation(self):
-        return self.bno055.euler
+        if self.bno055_isPresent:
+            return self.bno055.euler
+        else:
+            return [0, 0, 0]
 
     def getTemperature(self):
-        return self.bmp280.temperature
+        if self.bmp280_isPresent:
+            return self.bmp280.temperature
+        else:
+            return 18
 
     def getPressure(self):
-        return self.bmp280.pressure
+        if self.bmp280_isPresent:
+            return self.bmp280.pressure
+        else:
+            return 1021.05
+
 
 
