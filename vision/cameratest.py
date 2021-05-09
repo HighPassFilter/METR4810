92cd052dd0ee96ce87aeea720acd9e633fb6b6ea
Olivia <dev1@bankedge.com>
2021-05-09 16:39:31 +1000

    vision pt 3

---
+++ vision/cameratest.py
@@ -0,0 +1,16 @@
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+from time import sleep
+import cv2
+
+camera = PiCamera()
+camera.framerate = 32
+raw = PiRGBArray(camera)
+sleep(0.1)
+for frame in camera.capture_continuous(raw, format="bgr", use_video_port=True):
+    image = frame.array
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+    raw.truncate(0)
+    if key == ord("q"):
+        break
