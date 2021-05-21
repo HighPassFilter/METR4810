650ceef54d9ccbc402857bb7dfe88e58e4376469
Olivia <dev1@bankedge.com>
2021-05-21 16:43:49 +1000

    updated centre position

--- ExampleStateMachine.py
+++ ExampleStateMachine.py
@@ -126,13 +126,13 @@ class StateMachine():
 
         self.controller.update_channel(self.THROTTLE_CHANNEL, self.throttleLevel)
         if self.throttleLevel < 2000:
-            self.throttleLevel += 1
+            self.throttleLevel += 20
         if abs(self.tele.getOrientation()[1] - self.initialOri[1]) <= 4 and abs(self.tele.getOrientation()[2] - self.initialOri[2]) <= 4:
             # If craft is level TODO calibrate levelness values
             # 0.621x + 883
             # centre = self.vision.get_center_target()
             # print(centre)
-            # if self.centre_pos[0] != 0:
+            # if centre[0] != 0:
             #     self.controller.update_channel(self.PITCH_CHANNEL, self.centre_pos - int(0.5*centre[1]))
             #     self.controller.update_channel(self.ROLL_CHANNEL, self.centre_pos + int(0.5*centre[0]))
             #     time.sleep(0.2)
